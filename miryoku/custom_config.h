6dbe76f376d72d61aa3713d68d67c5601ecd726d
Grant-Helwig <39707361+Grant-Helwig@users.noreply.github.com>
2022-11-06 19:10:17 -0800

    added tap and tap back

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,11 +4,6 @@
 #if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
 
 #define XXX &none
-// #define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
-// &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-// &hm LGUI A,        &hm LALT R,        &hm LCTRL S,       &hm LSHFT T,       &kp G,             &kp M,             &hm LSHFT N,       &hm LCTRL E,       &hm LALT I,        &hm LGUI O,        \
-// &lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &hm RALT DOT,      &lt U_BUTTON SLASH,\
-// U_NP,              U_NP,              &lt U_MEDIA ESC,   &lt U_NAV SPC,     &lt U_MOUSE TAB,   &lt U_SYM RET,     &lt U_NUM BSPC,    &lt U_FUN DEL,     U_NP,              U_NP
 
 #define MIRYOKU_LAYERMAPPING_BASE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -16,9 +11,20 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp LSHFT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-&kp LCTRL  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+&lt U_TAP  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp C_VOL_UP \
+&kp CAPSLOCK  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp C_VOL_DN \
+&kp LSHFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp C_PP \
+               K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_TAP( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&lt U_BASE K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp C_VOL_UP \
+&kp CAPSLOCK  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp C_VOL_DN \
+&kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp C_PP \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
@@ -38,3 +44,5 @@
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_BASE
+
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_TAP
